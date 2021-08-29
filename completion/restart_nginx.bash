239c6a1cccbce481f299ccb202265727b35e03e1
Kori Syusetu <fumi11224@gmail.com>
2021-08-30 01:54:56 +0900

    動作確認Good

--- completion/restart_nginx.bash
+++ completion/restart_nginx.bash
@@ -1,9 +1,11 @@
 _restart_nginx() {
-	local cur prev words cword split
-	_init_completion || return
-	case $cword in
-		1)
-			COMPREPLY=( $(compgen -W '-h -l -k -r -s -e -d -i' -- "$cur") )
-			;;
+    local cur prev words cword split
+    _init_completion || return
+
+    case $cword in
+        1)
+            COMPREPLY=("-h" "-l" "-k" "-r" "-s" "-e" "-d" "-i")
+            ;;
+    esac
 }
 complete -F _restart_nginx restart_nginx
